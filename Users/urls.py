17225049dcda0d9ec783bd643cdadbfebbdbb4ff
hamodcraft22 <67144555+hamodcraft22@users.noreply.github.com>
2022-12-22 18:10:56 +0300

    minor fixes with links

--- Users/urls.py
+++ Users/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from Users import views
 
-urlpatterns = [path("register/", views.register, name='register')]
+urlpatterns = [path("accounts/register/", views.register, name='register')]
