80373317524d25806b6f6a421fed41424126ba92
hamodcraft22 <67144555+hamodcraft22@users.noreply.github.com>
2022-12-24 14:08:15 +0300

    all done an complete - clean without migrations

--- Users/urls.py
+++ Users/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from Users import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("accounts/register/", views.register, name='register')]
+    path("accounts/register/", views.register, name='register'),
+    path("accounts/profile/", auth_views.PasswordChangeView.as_view(template_name="registration/profile.html", success_url = reverse_lazy('home')), name="profile")]
